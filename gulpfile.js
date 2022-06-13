88c047218af9ca0a06c1a0bc86c3f8015120a4d2
Uzerano <luisadan.largamil.com>
2022-06-13 12:23:18 +0200

    Agregando dependencias GULP

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-const { src, dest } = require('gulp');
+
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass') (require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css( done ) {
 /// compilar sass
@@ -7,12 +10,23 @@ function css( done ) {
       
 src('src/scss/app.scss')
     .pipe( sass() )
+    .pipe(postcss( [ autoprefixer() ] ))
     .pipe( dest('build/css'))
 
     done();
+}
+
+function dev() {
+        watch('src/scss/app.scss', css);
 }
 
 
 
 
 exports.css = css;
+exports.dev = dev;
+exports.default = series( css, dev );
+
+//series -> Inicia las tareas de una en una, empieza con la primera termina y continua con la siguiente.
+
+//parallel -> Todas las tareas se inician al mismo tiempo.
