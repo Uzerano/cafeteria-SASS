0d5a69724ed99c039474a8f996b64183a8e1575f
Uzerano <luisadan.largamil.com>
2022-06-14 13:49:33 +0200

    Iniciando el Header

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,63 @@
+const { src, dest, watch, series, parallel } = require("gulp");
 
-const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass') (require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+// CSS y SASS
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
-function css( done ) {
-/// compilar sass
-///  pasos: 1- identificar archivo. 2- Compilar archivo. 3-Guardar en el archivo .css.
-      
-src('src/scss/app.scss')
-    .pipe( sass() )
-    .pipe(postcss( [ autoprefixer() ] ))
-    .pipe( dest('build/css'))
+ /// IMAGENES
+ const imagemin = require("gulp-imagemin");
+ const webp  = require("gulp-webp");
+ const avif  = require("gulp-avif");
 
+function css(done) {
+  /// compilar sass
+  ///  pasos: 1- identificar archivo. 2- Compilar archivo. 3-Guardar en el archivo .css.
+
+  src("src/scss/app.scss")
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest("build/css"));
+
+  done();
+}
+
+function imagenes( done ) {
+   src("src/img/**/*")
+    .pipe( imagemin( { optimizationLevel: 3} ) ) 
+    .pipe(dest('build/img'))
     done();
 }
 
-function dev() {
-        watch( 'src/scss/**/*.scss', css);
-    
+function versionWebp() {
+  const opciones = {
+    quality: 50
+  }
+  return src('src/img/**/*.{jpg,png}')
+  .pipe(webp( opciones ))
+  .pipe(dest('build/img'));
 }
 
+function versionAvif() {
+  const opciones = {
+    quality: 50
+  }
+   return src('src/img/**/*.{jpg,png}')
+   .pipe(avif( opciones ))
+   .pipe(dest('build/img'));
+}
 
-
+function dev() {
+  watch("src/scss/**/*.scss", css);
+  watch('src/img/**/*', imagenes)
+}
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev );
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.VersionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 
 //series -> Inicia las tareas de una en una, empieza con la primera termina y continua con la siguiente.
 
